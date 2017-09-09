996ebd0f1b0f8f3c6d94d452815a9ac4bbc32b22
Telei <tinglei_huang@163.com>
2017-09-09 23:33:28 +0800

    修改bug

--- src/clientMain.java
+++ src/clientMain.java
@@ -11,10 +11,11 @@ import java.util.regex.Pattern;
 public class clientMain {
 
     public static void main(String[] args) throws Exception{
-        Map<String,String> orders=new HashMap<>();//场馆使用
+        Map<String,List<String>>orders=new HashMap<>();//场馆使用
         Map<String,List<OrderDetails>> ordersMap=new HashMap<>();//订单详情
         Map<String,String> customer=new HashMap<>();
         List<OrderDetails> odList=null;
+        List<String> listTime=new ArrayList<>();
         ordersMap.put("A",odList);
         ordersMap.put("B",odList);
         ordersMap.put("C",odList);
@@ -61,7 +62,7 @@ public class clientMain {
                         String[] periodArr=input[2].split("~");
                         String startTime=input[1]+" "+periodArr[0];
                         String endTime=input[1]+" "+periodArr[1];
-                        String period=orders.get(input[3]+startTime+endTime);
+                        List<String>  period=orders.get(input[3]);
                         if(!wholePoint(periodArr[0]) || !wholePoint(periodArr[1])){
                             System.out.println("> Error:The booking is not the whole point!");
                         }else if(period!=null&&!RepeatedVerification(startTime,endTime,period)){
@@ -69,7 +70,8 @@ public class clientMain {
                         }else if(!timePeriod(periodArr[0],periodArr[1])){
                             System.out.println(">Error: Not in business hours or the end time is earlier than the start time!");
                         }else{
-                            orders.put(input[3]+startTime+endTime,startTime+"--"+endTime);
+                            listTime.add(startTime+"--"+endTime);
+                            orders.put(input[3],listTime);
                             customer.put(input[0]+input[1]+input[2],input[3]);
                             Site site=new Site();
                            int money=site.calculateCost(input[1],periodArr[0],periodArr[1],false);
@@ -103,7 +105,15 @@ public class clientMain {
                                 System.out.println("> Error:The booking being cancelled does not exist!");
                             }else if(booked.equals(input[3])){
                                 customer.remove(input[0]+input[1]+input[2]);
-                                orders.remove(input[3]+startTime+endTime);
+                                List<String> periods=orders.get(input[3]);
+                                Iterator<String> stringIterator=periods.iterator();
+                                while (stringIterator.hasNext()){
+                                    if(stringIterator.next().equals(startTime+"--"+endTime)){
+                                        stringIterator.remove();
+                                        break;
+                                    }
+                                }
+                                orders.remove(input[3]+input[1]+input[2]);
                                 Site site=new Site();
                                 int money=site.calculateCost(input[1],periodArr[0],periodArr[1],true);
                                 if(ordersMap.get(input[3])!=null){
@@ -140,16 +150,26 @@ public class clientMain {
 
     }
 
-    public static boolean RepeatedVerification(String startTime,String endTime,String period){
+    /**
+     * 比较时间重叠
+     * @param startTime
+     * @param endTime
+     * @param periods
+     * @return
+     */
+
+    public static boolean RepeatedVerification(String startTime,String endTime, List<String> periods){
         SimpleDateFormat sdf=new SimpleDateFormat("yyyy-MM-dd HH:mm");
         try {
-            String dateArr[]=period.split("--");
-            Date currDateStart = sdf.parse(startTime);//当前时间开始
-            Date currDateEnd = sdf.parse(endTime);//当前时间结束
-            Date startDate = sdf.parse(dateArr[0]);//每节开始时间
-            Date endDate = sdf.parse(dateArr[1]);//每节结束时间
-            if((currDateStart.getTime()==startDate.getTime()&&currDateEnd.getTime()==endDate.getTime())||(currDateStart.after(startDate) && currDateStart.before(endDate)) || (currDateEnd.after(startDate)&&currDateEnd.before(endDate))){
-                return false;
+            for (String period : periods){
+                String dateArr[]=period.split("--");
+                Date currDateStart = sdf.parse(startTime);//当前时间开始
+                Date currDateEnd = sdf.parse(endTime);//当前时间结束
+                Date startDate = sdf.parse(dateArr[0]);//每节开始时间
+                Date endDate = sdf.parse(dateArr[1]);//每节结束时间
+                if((currDateStart.getTime()==startDate.getTime()&&currDateEnd.getTime()==endDate.getTime())||(currDateStart.after(startDate) && currDateStart.before(endDate)) || (currDateEnd.after(startDate)&&currDateEnd.before(endDate))){
+                    return false;
+                }
             }
         } catch (ParseException e) {
             e.printStackTrace();
